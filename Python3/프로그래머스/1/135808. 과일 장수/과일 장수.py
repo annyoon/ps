5d6d1858975e030319f7d82fae8a73124211f877
Seoan Yoon <earthdyoon@gmail.com>
2025-07-31 23:48:46 +0900

    [level 1] Title: 과일 장수, Time: 99.50 ms, Memory: 20.7 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,7 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse = True)
+    for i in range(0, len(score), m):
+        if i + m <= len(score):
+            answer += m * (score[i + m - 1])
+    return answer
