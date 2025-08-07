37840ea4c136f4a5583fec77e09f35e3b669516f
Seoan Yoon <earthdyoon@gmail.com>
2025-08-08 00:41:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.01 ms, Memory: 33 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,14 @@
+def solution(participant, completion):
+    dic = {}
+    for p in participant:
+        if p in dic:
+            dic[p] += 1
+        else:
+            dic[p] = 1
+    
+    for c in completion:
+        dic[c] -= 1
+        
+    for d in dic:
+        if dic[d] == 1:
+            return d
