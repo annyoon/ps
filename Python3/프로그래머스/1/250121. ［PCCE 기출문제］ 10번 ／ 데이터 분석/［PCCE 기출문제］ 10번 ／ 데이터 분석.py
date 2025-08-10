718e511480e7e49f2425e371703dbd8a23618ee3
Seoan Yoon <earthdyoon@gmail.com>
2025-08-10 22:57:46 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.24 ms, Memory: 9.42 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,9 @@
+def solution(data, ext, val_ext, sort_by):
+    dic = {'code': 0, 'date': 1, 'maximum': 2, 'remain': 3}
+    answer = []
+    
+    for d in data:
+        if d[dic[ext]] < val_ext:
+            answer.append(d)
+            
+    return sorted(answer, key = lambda x: x[dic[sort_by]])
